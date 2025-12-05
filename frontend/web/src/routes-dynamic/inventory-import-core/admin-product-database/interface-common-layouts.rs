9d1bc6ced5bc4d059bfec560e5188539e3e38fa5
marthaediazx <tolik-z134804845@rambler.ru>
2025-12-05 10:49:45 +0800

    add dist.java, CsrFeedbackFHE__factory.ts, tsconfig.json

---
+++ frontend/web/src/routes-dynamic/inventory-import-core/admin-product-database/interface-common-layouts.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\routes-dynamic\inventory-import-core\admin-product-database\interface-common-layouts.rs
+// Generated at: 2025-10-25T11:42:14.179Z
